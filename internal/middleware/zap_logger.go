2b88ffc53a094b457c2e2231fc5a9035d2a9f331
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-08 18:37:31 +0700

    feat(logger): add ZapLogger middleware for structured logging of HTTP requests

---
+++ internal/middleware/zap_logger.go
@@ -0,0 +1,31 @@
+package config
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5/middleware"
+	"go.uber.org/zap"
+)
+
+func ZapLogger(log *zap.Logger) func(next http.Handler) http.Handler {
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			ww := middleware.NewWrapResponseWriter(w, r.ProtoMajor)
+			t1 := time.Now()
+
+			defer func() {
+				log.Info("request completed",
+					zap.String("method", r.Method),
+					zap.String("path", r.URL.Path),
+					zap.Int("status", ww.Status()),
+					zap.Duration("latency", time.Since(t1)),
+					zap.Int("size", ww.BytesWritten()),
+					zap.String("remote_ip", r.RemoteAddr),
+				)
+			}()
+
+			next.ServeHTTP(ww, r)
+		})
+	}
+}

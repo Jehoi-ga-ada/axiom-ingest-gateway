97690b52a8aa48895a0b86b670bb65dcc2a4dfb1
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-08 18:37:42 +0700

    feat(config): add NewApp function and Config struct for application setup

---
+++ internal/config/app.go
@@ -0,0 +1,26 @@
+package config
+
+import (
+	ucEvent "github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/ingest/application/usecase"
+	eventHttp "github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/ingest/delivery/http"
+	"github.com/go-chi/chi/v5"
+	"github.com/spf13/viper"
+	"go.uber.org/zap"
+)
+
+type Config struct {
+	Router chi.Router
+	Viper *viper.Viper
+	Logger *zap.Logger
+}
+
+func NewApp(config *Config) {
+	eventIngester := ucEvent.NewEventIngester()
+	eventHandler := eventHttp.NewEventHandler(eventIngester)
+
+	config.Router.Route("/api/v1", func(r chi.Router) {
+		r.Route("/events", func(r chi.Router) {
+			eventHandler.Register(r)
+		})
+	})
+}

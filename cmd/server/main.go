1acb2d1bfbea81d9e31e43145b411b919da8f292
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-08 18:37:38 +0700

    feat(server): refactor main function to initialize logger, viper, and server with proper configurations

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,16 +1,58 @@
 package main
 
 import (
-	"log"
+	"fmt"
 	"net/http"
+	"time"
+
+	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/config"
+	"go.uber.org/zap"
 )
 
 func main() {
-	http.HandleFunc("/health", func(w http.ResponseWriter, _ *http.Request) {
-		w.WriteHeader(http.StatusOK)
-		w.Write([]byte("ok"))
+	logger, err := config.NewLogger()
+	if err != nil {
+		fmt.Printf("Failed to initialize logger: %s\n", err.Error())
+	}
+
+	viper, err := config.NewViper()
+	if err != nil {
+		logger.Fatal("Viper failed to unload",
+			zap.String("err", err.Error()),
+		)
+	}
+
+	router := config.NewRouter(logger)
+
+	config.NewApp(&config.Config{
+		Router: router,
+		Viper: viper,
+		Logger: logger,
 	})
 
-	log.Println("ingest-gateway listening on :8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	port := viper.GetString("PORT")
+	if port == "" {
+		logger.Info("PORT not found, defaulting to :8000")
+		port = "8000"
+	}
+
+	serverAddr := fmt.Sprintf(":%s", port)
+	logger.Info("Starting server on",
+		zap.String("port", port),
+	)
+
+	server := &http.Server{
+		Addr: serverAddr,
+		Handler: router,
+		ReadTimeout: 15 * time.Second,
+		WriteTimeout: 15 * time.Second,
+		IdleTimeout: 15 * time.Second,
+	}
+
+	err = server.ListenAndServe()
+	if err != nil {
+		logger.Fatal("Server failed to start",
+			zap.String("err", err.Error()),
+		)
+	}
 }

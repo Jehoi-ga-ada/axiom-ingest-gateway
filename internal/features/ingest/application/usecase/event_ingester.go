94f7f73f8ca564d2eda12ca997301a5a1772f95c
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-09 18:52:15 +0700

    feat(eventIngester): pass logger to NewEventIngester for improved error logging

--- internal/features/ingest/application/usecase/event_ingester.go
+++ internal/features/ingest/application/usecase/event_ingester.go
@@ -5,20 +5,26 @@ import (
 
 	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/ingest/delivery/dto"
 	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/ingest/domain"
+	"go.uber.org/zap"
 )
 
 type eventIngester struct {
-	
+	logger *zap.Logger
 }
 
-func NewEventIngester() EventIngester {
-	return &eventIngester{}
+func NewEventIngester(logger *zap.Logger) EventIngester {
+	return &eventIngester{
+		logger: logger,
+	}
 }
 
 func (u *eventIngester) Execute(ctx context.Context, req dto.CreateEventRequest) (string, error) {
 	e, err := domain.NewEvent(req.Type, req.Timestamp, req.RawBody)
 
 	if err != nil {
+		u.logger.Fatal("Failed to create event with error",
+			zap.String("error", err.Error()),
+		)
 		return "", err
 	}
 

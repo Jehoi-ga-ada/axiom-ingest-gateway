f4725b06bf3699c84b910b3b6146bba7aba56559
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-11 19:52:35 +0700

    feat(ingester): refactor Execute method to use fasthttp.RequestCtx and add dispatcher integration

--- internal/features/ingest/application/usecase/event_ingester.go
+++ internal/features/ingest/application/usecase/event_ingester.go
@@ -1,11 +1,10 @@
 package usecase
 
 import (
-	"context"
-
 	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/ingest/delivery/dto"
 	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/ingest/domain"
 	v1 "github.com/Jehoi-ga-ada/axiom-schema/gen/go/v1"
+	"github.com/valyala/fasthttp"
 	"go.uber.org/zap"
 	"google.golang.org/protobuf/proto"
 	"google.golang.org/protobuf/types/known/timestamppb"
@@ -13,6 +12,7 @@ import (
 
 type eventIngester struct {
 	logger *zap.Logger
+	dispatcher domain.EventDispatcher
 }
 
 func NewEventIngester(logger *zap.Logger) EventIngester {
@@ -21,7 +21,7 @@ func NewEventIngester(logger *zap.Logger) EventIngester {
 	}
 }
 
-func (u *eventIngester) Execute(ctx context.Context, req dto.CreateEventRequest) (string, error) {
+func (u *eventIngester) Execute(ctx *fasthttp.RequestCtx, req dto.CreateEventRequest) (string, error) {
 	e, err := domain.NewEvent(req.Type, req.Timestamp, req.RawBody)
 
 	if err != nil {
@@ -39,7 +39,7 @@ func (u *eventIngester) Execute(ctx context.Context, req dto.CreateEventRequest)
 		RawBody: e.RawBody,
 	}
 
-	_, err = proto.Marshal(pb)
+	data, err := proto.Marshal(pb)
 	if err != nil {
 		u.logger.Error("failed to serialize event",
 			zap.Error(err),
@@ -47,7 +47,10 @@ func (u *eventIngester) Execute(ctx context.Context, req dto.CreateEventRequest)
 		return "", err
 	}
 
-	// TODO: Handoff to dispatcher
+	if err := u.dispatcher.Enqueue(ctx, data); err != nil {
+		u.logger.Error("dispatcher queue full", zap.Error(err))
+		return "", err
+	}
 
 	return e.ID.String(), nil
 }

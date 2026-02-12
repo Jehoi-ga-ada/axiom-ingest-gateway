30a56754e91e784e6de7564e90a7facd3226603f
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-12 21:00:37 +0700

    refactor(dispatcher): simplify Enqueue method signature by removing context parameter

--- internal/features/ingest/domain/interfaces.go
+++ internal/features/ingest/domain/interfaces.go
@@ -1,8 +1,6 @@
 package domain
 
-import "github.com/valyala/fasthttp"
-
 type EventDispatcher interface {
-	Enqueue(ctx *fasthttp.RequestCtx, data []byte) error
+	Enqueue(data []byte) error
 	Close()
 }

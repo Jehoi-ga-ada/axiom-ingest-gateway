5c607f178c0bfa32235ddf15306594800495a25c
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-14 15:57:50 +0700

    feat(auth): implement AuthMiddleware for API key validation

---
+++ internal/middleware/auth.go
@@ -0,0 +1,30 @@
+package middleware
+
+import (
+	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/auth/application/service"
+	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/shared/utils"
+	"github.com/valyala/fasthttp"
+)
+
+func AuthMiddleware(registry service.KeyRegistry) func(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+	headerKey := []byte("X-API-Key")
+	return func(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+		return func (ctx *fasthttp.RequestCtx) {
+			key := ctx.Request.Header.PeekBytes(headerKey)
+
+			if len(key) == 0 {
+				utils.StatusUnauthorized(ctx, "API key not found")
+				return
+			}
+
+			meta, ok := registry.Get(key)
+			if !ok || !meta.Active{
+				utils.StatusUnauthorized(ctx, "API key not found")
+				return
+			}
+			
+			ctx.SetUserValue("tenant_id", meta.TenantID)
+        	next(ctx)
+		}
+	}
+}

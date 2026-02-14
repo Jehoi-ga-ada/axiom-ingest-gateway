93cfb0f38add876d8b413e20feff66a6475db95c
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-14 16:46:35 +0700

    fix(auth): change Get method to return pointer to APIKeyMetadata

--- internal/features/auth/application/service/interface.go
+++ internal/features/auth/application/service/interface.go
@@ -3,6 +3,6 @@ package service
 import "github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/auth/domain"
 
 type KeyRegistry interface {
-	Get(key []byte) (domain.APIKeyMetadata, bool)
+	Get(key []byte) (*domain.APIKeyMetadata, bool)
 	Upsert(key string, metadata domain.APIKeyMetadata)
 }

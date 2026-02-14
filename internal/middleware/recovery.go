f79f02e8f6bb8ebcfefd4ce9010302a3bf1d2c57
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-14 15:23:30 +0700

    refactor(recovery): update RecoveryMiddleware to return a closure for improved handler chaining

--- internal/middleware/recovery.go
+++ internal/middleware/recovery.go
@@ -8,19 +8,21 @@ import (
 	"go.uber.org/zap"
 )
 
-func RecoveryMiddleware(log *zap.Logger, next fasthttp.RequestHandler) fasthttp.RequestHandler {
-    return func(ctx *fasthttp.RequestCtx) {
-        defer func() {
-            if r := recover(); r != nil {
-                log.Error("recovered from panic",
-                    zap.Any("error", r),
-                    zap.ByteString("stack", debug.Stack()),
-                    zap.ByteString("path", ctx.Path()),
-                )
+func RecoveryMiddleware(log *zap.Logger) func (next fasthttp.RequestHandler) fasthttp.RequestHandler {
+    return func(next fasthttp.RequestHandler) fasthttp.RequestHandler {
+        return func(ctx *fasthttp.RequestCtx) {
+            defer func() {
+                if r := recover(); r != nil {
+                    log.Error("recovered from panic",
+                        zap.Any("error", r),
+                        zap.ByteString("stack", debug.Stack()),
+                        zap.ByteString("path", ctx.Path()),
+                    )
 
-                utils.StatusInternalServerError(ctx, "internal server error")
-            }
-        }()
-        next(ctx)
+                    utils.StatusInternalServerError(ctx, "internal server error")
+                }
+            }()
+            next(ctx)
+        }
     }
 }

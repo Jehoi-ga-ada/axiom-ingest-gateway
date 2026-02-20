f09e9c69374593ef3475c918639636c30c0cd172
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-20 19:05:37 +0700

    feat(dispatcher): implement udsDispatcher for Unix domain socket communication

---
+++ internal/features/ingest/application/infrastructure/uds_dispatcher.go
@@ -0,0 +1,196 @@
+package infrastructure
+
+import (
+	"encoding/binary"
+	"net"
+	"sync"
+	"time"
+
+	"github.com/Jehoi-ga-ada/axiom-ingest-gateway/internal/features/ingest/domain"
+	"go.uber.org/zap"
+)
+
+type udsDispatcher struct {
+	logger   *zap.Logger
+	config   DispatcherConfig
+	queue    chan []byte
+	outbound chan [][]byte
+	bytePool *sync.Pool
+	batchPool *sync.Pool 
+	workerWG sync.WaitGroup
+	senderWG sync.WaitGroup 
+	stop     chan struct{}
+}
+
+func NewUDSDispatcher(logger *zap.Logger, cfg DispatcherConfig) EventDispatcher {
+	d := &udsDispatcher{
+		logger:   logger,
+		config:   cfg,
+		queue:    make(chan []byte, cfg.QueueSize),
+		outbound: make(chan [][]byte, cfg.MaxWorkers),
+		stop:     make(chan struct{}),
+		bytePool: &sync.Pool{
+			New: func() interface{} {
+				return make([]byte, cfg.BufferMaxSize)
+			},
+		},
+		batchPool: &sync.Pool{
+			New: func() interface{} {
+				return make([][]byte, 0, cfg.BatchSize)
+			},
+		},
+	}
+
+	for i := 0; i < cfg.MaxSenders; i ++ {
+		d.senderWG.Add(1)
+		go d.sender()
+	}
+
+	for i := 0; i < cfg.MaxWorkers; i++ {
+		d.workerWG.Add(1)
+		go d.worker()
+	}
+
+	return d
+}
+
+func (d *udsDispatcher) Enqueue(data []byte) error {
+	if len(data) > d.config.BufferMaxSize {
+		return domain.ErrPayloadTooLarge
+	}
+
+	buf := d.bytePool.Get().([]byte)
+	n := copy(buf, data)
+	item := buf[:n]
+
+	select {
+	case d.queue <- item:
+		return nil
+	default:
+		d.bytePool.Put(buf[:cap(buf)])
+		return domain.ErrDispatchQueueIsFull
+	}
+}
+
+func (d *udsDispatcher) worker() {
+	defer d.workerWG.Done()
+
+	batch := d.batchPool.Get().([][]byte)
+	batch = batch[:0]
+	ticker := time.NewTicker(d.config.FlushInterval)
+	defer ticker.Stop()
+
+	for {
+		select {
+		case item, ok := <-d.queue:
+			if !ok {
+				if len(batch) > 0 {
+					d.outbound <- batch
+				} else {
+					d.batchPool.Put(batch)
+				}
+				return
+			}
+
+			batch = append(batch, item)
+
+			if len(batch) >= d.config.BatchSize {
+				d.outbound <- batch
+				batch = d.batchPool.Get().([][]byte)
+			}
+		case <-ticker.C:
+			if len(batch) > 0 {
+				d.outbound <- batch
+				batch = d.batchPool.Get().([][]byte)
+			}
+		case <-d.stop:
+			if len(batch) > 0 {
+				d.outbound <- batch
+			} else {
+				d.batchPool.Put(batch)
+			}
+			return
+		}
+	}
+}
+
+func (d *udsDispatcher) sender() {
+	defer d.senderWG.Done()
+	var conn net.Conn
+	ackBuf := make([]byte, 1)
+
+	headerPool := make([]byte, d.config.BatchSize*4)
+
+	cleanup := func() {
+		if conn != nil {
+			conn.Close()
+			conn = nil
+		}
+	}
+
+	vBuffers := make(net.Buffers, 0, d.config.BatchSize*2)
+	for batch := range d.outbound {
+		vBuffers = vBuffers[:0]
+		
+		if conn == nil {
+			var err error
+			conn, err = net.DialTimeout("unix", d.config.TargetAddr, 3*time.Second)
+			if err != nil {
+				d.logger.Error("uds dial failed", zap.Error(err))
+				d.releaseBatch(batch)
+				time.Sleep(500 * time.Millisecond)
+				continue
+			} else if unixConn, ok := conn.(*net.UnixConn); ok {
+				_ = unixConn.SetWriteBuffer(16 * 1024 * 1024)
+				_ = unixConn.SetReadBuffer(16 * 1024 * 1024)
+			}
+		}
+
+		conn.SetWriteDeadline(time.Now().Add(d.config.WriteTimeout))
+		
+		for i, msg := range batch {
+			start := i * 4
+			header := headerPool[start : start+4]
+			binary.BigEndian.PutUint32(header, uint32(len(msg)))
+			vBuffers = append(vBuffers, header, msg)
+		}
+
+		_, err := vBuffers.WriteTo(conn)
+
+		if err == nil {
+			conn.SetReadDeadline(time.Now().Add(d.config.WriteTimeout))
+			_, err = conn.Read(ackBuf)
+		}
+
+		if err != nil {
+			d.logger.Error("uds transport error", zap.Error(err))
+			cleanup()
+		}
+		
+		d.releaseBatch(batch)
+	}
+	cleanup()
+}
+
+func (d *udsDispatcher) releaseBatch(batch [][]byte) {
+	for i := range batch {
+		if batch[i] != nil {
+			d.bytePool.Put(batch[i][:cap(batch[i])])
+			batch[i] = nil
+		}
+	}
+
+	d.batchPool.Put(batch[:0])
+}
+
+func (d *udsDispatcher) Close() {
+	d.logger.Info("shutting down dispatcher")
+	
+	close(d.stop)
+	close(d.queue)
+	d.workerWG.Wait()
+	close(d.outbound)
+	d.senderWG.Wait()
+	
+	d.logger.Info("dispatcher closed gracefully")
+}

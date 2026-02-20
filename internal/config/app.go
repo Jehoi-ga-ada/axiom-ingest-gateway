9a37408a28f979d1e785451ce38eeb200485b175
Jehoi-ga-ada <sayajehoiada@gmail.com>
2026-02-20 19:06:13 +0700

    perf(dispatcher): increase MaxSenders to improve event handling capacity

--- internal/config/app.go
+++ internal/config/app.go
@@ -22,13 +22,15 @@ func NewApp(config *Config) eventInfra.EventDispatcher {
 		BatchSize: 1000,
 		FlushInterval: 10 * time.Millisecond,
 		MaxWorkers: 10,
-		MaxSenders: 1,
+		MaxSenders: 6,
 		QueueSize: 50000,
 		BufferMaxSize: 1024 * 1024,
 		TargetAddr: "127.0.0.1:8080",
+		// TargetAddr: "/tmp/axiom.sock",
 		WriteTimeout: 5 * time.Second,
 	}
 	eventDispatcher := eventInfra.NewTCPDispatcher(config.Logger, dispatcherConfig)
+	// eventDispatcher := eventInfra.NewUDSDispatcher(config.Logger, dispatcherConfig)
 	eventIngester := ucEvent.NewEventIngester(config.Logger, eventDispatcher)
 	eventHandler := eventHttp.NewEventHandler(eventIngester)
 
